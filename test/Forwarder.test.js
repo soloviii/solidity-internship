d40acb78fd3dfc0452762009dbd2ff0010a1225f
Vasyl Solovii <soloviy.qwerty@gmail.com>
2021-12-03 18:11:12 +0200

    added sign and verify message

--- test/Forwarder.test.js
+++ test/Forwarder.test.js
@@ -1,69 +1,107 @@
-const {
-    BN,
-    send,
-    constants,
-    expectRevert,
-    snapshot,
-    expectEvent,
-    balance,
-    ether
-} = require('@openzeppelin/test-helpers');
-const { expect } = require('chai');
-require('chai')
-    .use(require('chai-as-promised'))
-    .use(require('chai-bignumber')())
-    .use(require('chai-bn')(BN))
-    .should();
+const { ethers } = require('hardhat')
+const { expect } = require('chai')
+const { ether } = require('@openzeppelin/test-helpers');
 
-const Forwarder = artifacts.require("Forwarder");
-let snapshotA;
+const DOMAON_TYPE = [{
+        type: "string",
+        name: "name"
+    },
+    {
+        type: "string",
+        name: "version"
+    },
+    {
+        type: "uint256",
+        name: "chainId"
+    },
+    {
+        type: "address",
+        name: "verifyingContract"
+    }
+];
 
-contract("Forwarder", ([owner, user1, user2]) => {
+let owner, user1, user2
+let forwarderFactory, forwarderInstance
 
-    before(async() => {
-        forwarderInstance = await Forwarder.new();
-        snapshotA = await snapshot();
-    });
+describe('Forwarder', async() => {
+    beforeEach(async() => {
+        [owner, user1, user2] = await ethers.getSigners()
+        forwarderFactory = await ethers.getContractFactory("Forwarder")
+        forwarderInstance = await forwarderFactory.deploy()
+        await forwarderInstance.setKycSigner(user1.address.toString())
+    })
 
-    afterEach(async() => {
-        await snapshotA.restore();
-    });
-
-    describe("functions", async() => {
-        describe("forward", async() => {
-            it("Should be fail if address destination is zero", async() => {
-                await expectRevert(
-                    forwarderInstance.forward(constants.ZERO_ADDRESS, "Information", { from: user1, value: ether('2') }),
-                    "forward to the zero address",
-                );
-            });
-            it("Should check correct transfer amount from user who transfer", async() => {
-                let beforeBalanceUser1 = await balance.current(user1);
-                let transaction = await forwarderInstance.forward(user2, "Information", { from: user1, value: ether('2') });
-                const tx = await web3.eth.getTransaction(transaction.tx);
-                const gasCost = new BN(tx.gasPrice).mul(new BN(transaction.receipt.gasUsed));
-                let newBalanceUser1 = await balance.current(user1);
-                expect(newBalanceUser1).to.be.bignumber.equal(beforeBalanceUser1.sub(gasCost).sub(ether('2')));
-            });
+    describe('functions', async() => {
+        describe('forward', async() => {
             it("Should deposit and forward ether to the destination address", async() => {
-                let beforeBalanceUser2 = await balance.current(user2);
-                await forwarderInstance.forward(user2, "Information", { from: user1, value: ether('2') });
-                let newBalanceUser2 = await balance.current(user2);
-                expect(newBalanceUser2).to.be.bignumber.equal(beforeBalanceUser2.add(ether('2')));
-            });
-        });
-        describe("events", async() => {
-            describe("LogForwarded", async() => {
-                it("emits a LogForwarded event on succesfull function forward", async() => {
-                    const receipt = await forwarderInstance.forward(user2, "Information", { from: user1, value: ether('2') });
-                    expectEvent(receipt, 'LogForwarded', {
-                        from: user1,
-                        to: user2,
-                        amount: ether('2'),
-                        data: "Information"
-                    });
-                });
-            });
-        });
-    });
-});
+                const { chainId } = await ethers.provider.getNetwork()
+                const TYPES = {
+                    Container: [{
+                            name: 'from',
+                            type: 'address',
+                        },
+                        {
+                            name: 'to',
+                            type: 'address',
+                        },
+                        {
+                            name: 'amount',
+                            type: 'uint256',
+                        },
+                        {
+                            name: 'nonce',
+                            type: 'uint256',
+                        },
+                        {
+                            name: 'data',
+                            type: 'string',
+                        },
+                    ],
+                }
+
+                let sign = await ethers.provider.send('eth_signTypedData_v4', [
+                    (await user1.address.toString()).toLowerCase(),
+                    JSON.stringify({
+                        types: Object.assign({
+                                EIP712Domain: DOMAON_TYPE,
+                            },
+                            TYPES,
+                        ),
+                        domain: {
+                            name: 'Forward',
+                            version: 'v1',
+                            chainId: chainId,
+                            verifyingContract: forwarderInstance.address,
+                        },
+                        primaryType: 'Container',
+                        message: {
+                            from: (await user1.address.toString()).toLowerCase(),
+                            to: (await user2.address.toString()).toLowerCase(),
+                            amount: ether('1').toString(),
+                            nonce: Number(await forwarderInstance.connect(user1).getNonce()),
+                            data: "data",
+                        },
+                    }),
+                ])
+
+                const sig = sign
+                const sig0 = sig.substring(2)
+                const r = '0x' + sig0.substring(0, 64)
+                const s = '0x' + sig0.substring(64, 128)
+                const v = parseInt(sig0.substring(128, 130), 16)
+
+                let nonce = Number(await forwarderInstance.connect(user1).getNonce())
+                const provider = waffle.provider
+                const beforeBalanceUser2 = await provider.getBalance(user2.address)
+
+                await expect(
+                    await forwarderInstance
+                    .connect(user1)
+                    .forward(user2.address, nonce, "data", v, r, s, { value: ether('1').toString() }),
+                )
+                const afterBalanceUser2 = await provider.getBalance(user2.address);
+                expect(afterBalanceUser2).to.equal(beforeBalanceUser2.add(ether('1').toString()));
+            })
+        })
+    })
+})
