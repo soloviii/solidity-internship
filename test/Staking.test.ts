83a8ee338401bf988b636e12176064650d41e2f5
Vasyl <vasyl.solovii@applicature.com>
2022-03-17 09:17:02 +0200

    uncommented tests

--- test/Staking.test.ts
+++ test/Staking.test.ts
@@ -53,111 +53,111 @@ describe('Staking', async () => {
         await rewardToken.approve(staking.address, toETH("500000"))
     })
     describe('Functions', async () => {
-        // describe('constructor', async () => {
-        //     it('should fail if reward token address is zero', async () => {
-        //         const Staking = await ethers.getContractFactory('Staking')
-        //         await expect(Staking.deploy(constants.ZERO_ADDRESS, constants.ZERO_ADDRESS)
-        //         ).to.be.revertedWith("ERROR_INVALID_ADDRESS")
-        //     })
-        //     it('should deploy Staking SC successfully', async () => {
-        //         const Staking = await ethers.getContractFactory('Staking')
-        //         staking = await Staking.deploy(rewardToken.address, stakingToken.address)
-        //         let stakingInfo = await staking.stakingInfo();
-        //         expect(stakingInfo.feePercentage).to.be.equal(40);
-        //         expect(stakingInfo.cooldown).to.be.equal(864000);
-        //         expect(stakingInfo.stakingPeriod).to.be.equal(5184000);
-        //     })
-        // })
-        // describe('setRewards', async () => {
-        //     it('should fail if sender is not owner', async () => {
-        //         let blo = await ethers.provider.getBlockNumber()
-        //         let now = (await ethers.provider.getBlock(blo)).timestamp
-        //         await expect(staking.connect(investor1).setRewards(now + 10, now + 20, "1", '10')
-        //         ).to.be.revertedWith("Ownable: caller is not the owner")
-        //     })
-        //     it('should fail if incorrect dates', async () => {
-        //         let blo = await ethers.provider.getBlockNumber()
-        //         let now = (await ethers.provider.getBlock(blo)).timestamp
-        //         await expect(staking.setRewards(now - 10, now + 20, "1", '10')
-        //         ).to.be.revertedWith("Incorrect dates")
-        //         await expect(staking.setRewards(now + 10, now, "1", '10')
-        //         ).to.be.revertedWith("Incorrect dates")
-        //     })
-        //     it('should fail if incorrect reward amount of tokens', async () => {
-        //         let blo = await ethers.provider.getBlockNumber()
-        //         let now = (await ethers.provider.getBlock(blo)).timestamp
-        //         await expect(staking.setRewards(now + 10, now + 20, toETH("500001"), '10')
-        //         ).to.be.revertedWith("Incorrect amount of tokens")
-        //         await expect(staking.setRewards(now + 10, now + 20, "0", '10')
-        //         ).to.be.revertedWith("Incorrect amount of tokens")
-        //     })
-        //     it('should fail if incorrect apy', async () => {
-        //         let blo = await ethers.provider.getBlockNumber()
-        //         let now = (await ethers.provider.getBlock(blo)).timestamp
-        //         await expect(staking.setRewards(now + 10, now + 20, "500000", '0')
-        //         ).to.be.revertedWith("Incorrect apy")
-        //         await expect(staking.setRewards(now + 10, now + 20, "500000", '11')
-        //         ).to.be.revertedWith("Incorrect apy")
-        //     })
-        //     it('should set reward info in Staking SC successfully', async () => {
-        //         await rewardToken.mint(owner.address, toETH("500000"))
-        //         await rewardToken.approve(staking.address, toETH("500000"))
-        //         let blo = await ethers.provider.getBlockNumber()
-        //         let now = (await ethers.provider.getBlock(blo)).timestamp
-        //         let beforeStakingBalance = await rewardToken.balanceOf(staking.address)
-        //         await expect(staking.setRewards(now + 10, now + 20, toETH("500000"), '10')
-        //         ).to.emit(staking, 'SetRewards').withArgs(now + 10, now + 20,
-        //             toETH("500000"), '10')
-        //         let afterStakingBalance = await rewardToken.balanceOf(staking.address)
-        //         expect(afterStakingBalance).to.be.equal(beforeStakingBalance.add(toETH("500000")))
-        //         expect(await staking.startDate()).to.be.equal(now + 10)
-        //         expect(await staking.endDate()).to.be.equal(now + 20)
-        //         expect(await staking.apy()).to.be.equal(10)
-        //     })
-        // });
-        // describe('stake', async () => {
-        //     it('should fail if staked amount is zero', async () => {
-        //         let blo = await ethers.provider.getBlockNumber()
-        //         let now = (await ethers.provider.getBlock(blo)).timestamp
-        //         await staking.setRewards(now + 10, now + 20, toETH("500000"), '10')
-        //         await expect(staking.connect(investor1).stake("0")
-        //         ).to.be.revertedWith("ERROR_AMOUNT_IS_ZERO")
-        //     })
-        //     it('should fail if staked amount is over max', async () => {
-        //         let blo = await ethers.provider.getBlockNumber()
-        //         let now = (await ethers.provider.getBlock(blo)).timestamp
-        //         await staking.setRewards(now + 10, now + 20, toETH("500000"), '10')
-        //         await stakingToken.mint(investor1.address, toETH("5000001"))
-        //         await stakingToken.connect(investor1).approve(staking.address, toETH("5000001"))
-        //         await expect(staking.connect(investor1).stake(toETH("5000001"))
-        //         ).to.be.revertedWith("ERROR_MAX_TOTAL_SUPPLY")
-        //     })
-        //     it('should fail if cooldown has not been passed', async () => {
-        //         let blo = await ethers.provider.getBlockNumber()
-        //         let now = (await ethers.provider.getBlock(blo)).timestamp
-        //         await staking.setRewards(now + 10, now + 20, toETH("500000"), '10')
-        //         await stakingToken.mint(investor1.address, toETH("5000000"))
-        //         await stakingToken.connect(investor1).approve(staking.address, toETH("5000000"))
-        //         await staking.connect(investor1).stake(toETH("2000000"))
-        //         await expect(staking.connect(investor1).stake(toETH("3000000"))
-        //         ).to.be.revertedWith("The cooldown has not been passed")
-        //     })
-        //     it('should stake the amount of staking tokens successfully', async () => {
-        //         let blo = await ethers.provider.getBlockNumber()
-        //         let now = (await ethers.provider.getBlock(blo)).timestamp
-        //         await staking.setRewards(now + 10, now + 30, toETH("500000"), '10')
-        //         await stakingToken.mint(investor1.address, toETH("3000"))
-        //         await stakingToken.connect(investor1).approve(staking.address, toETH("3000"))
-        //         let beforeStakingBalance = await stakingToken.balanceOf(staking.address)
-        //         await expect(staking.connect(investor1).stake(toETH("3000"))
-        //         ).to.emit(staking, 'Stake').withArgs(investor1.address, toETH("3000"))
-        //         let afterStakingBalance = await stakingToken.balanceOf(staking.address)
-        //         expect(afterStakingBalance).to.be.equal(beforeStakingBalance.add(toETH("3000")))
-        //         expect(await staking.totalSupply()).to.be.equal(toETH("3000"))
-        //         expect(await staking.staked(investor1.address)).to.be.equal(toETH("3000"))
-        //         expect(await staking.rewards(investor1.address)).to.be.equal(toETH("0"))
-        //     })
-        // });
+        describe('constructor', async () => {
+            it('should fail if reward token address is zero', async () => {
+                const Staking = await ethers.getContractFactory('Staking')
+                await expect(Staking.deploy(constants.ZERO_ADDRESS, constants.ZERO_ADDRESS)
+                ).to.be.revertedWith("ERROR_INVALID_ADDRESS")
+            })
+            it('should deploy Staking SC successfully', async () => {
+                const Staking = await ethers.getContractFactory('Staking')
+                staking = await Staking.deploy(rewardToken.address, stakingToken.address)
+                let stakingInfo = await staking.stakingInfo();
+                expect(stakingInfo.feePercentage).to.be.equal(40);
+                expect(stakingInfo.cooldown).to.be.equal(864000);
+                expect(stakingInfo.stakingPeriod).to.be.equal(5184000);
+            })
+        })
+        describe('setRewards', async () => {
+            it('should fail if sender is not owner', async () => {
+                let blo = await ethers.provider.getBlockNumber()
+                let now = (await ethers.provider.getBlock(blo)).timestamp
+                await expect(staking.connect(investor1).setRewards(now + 10, now + 20, "1", '10')
+                ).to.be.revertedWith("Ownable: caller is not the owner")
+            })
+            it('should fail if incorrect dates', async () => {
+                let blo = await ethers.provider.getBlockNumber()
+                let now = (await ethers.provider.getBlock(blo)).timestamp
+                await expect(staking.setRewards(now - 10, now + 20, "1", '10')
+                ).to.be.revertedWith("Incorrect dates")
+                await expect(staking.setRewards(now + 10, now, "1", '10')
+                ).to.be.revertedWith("Incorrect dates")
+            })
+            it('should fail if incorrect reward amount of tokens', async () => {
+                let blo = await ethers.provider.getBlockNumber()
+                let now = (await ethers.provider.getBlock(blo)).timestamp
+                await expect(staking.setRewards(now + 10, now + 20, toETH("500001"), '10')
+                ).to.be.revertedWith("Incorrect amount of tokens")
+                await expect(staking.setRewards(now + 10, now + 20, "0", '10')
+                ).to.be.revertedWith("Incorrect amount of tokens")
+            })
+            it('should fail if incorrect apy', async () => {
+                let blo = await ethers.provider.getBlockNumber()
+                let now = (await ethers.provider.getBlock(blo)).timestamp
+                await expect(staking.setRewards(now + 10, now + 20, "500000", '0')
+                ).to.be.revertedWith("Incorrect apy")
+                await expect(staking.setRewards(now + 10, now + 20, "500000", '11')
+                ).to.be.revertedWith("Incorrect apy")
+            })
+            it('should set reward info in Staking SC successfully', async () => {
+                await rewardToken.mint(owner.address, toETH("500000"))
+                await rewardToken.approve(staking.address, toETH("500000"))
+                let blo = await ethers.provider.getBlockNumber()
+                let now = (await ethers.provider.getBlock(blo)).timestamp
+                let beforeStakingBalance = await rewardToken.balanceOf(staking.address)
+                await expect(staking.setRewards(now + 10, now + 20, toETH("500000"), '10')
+                ).to.emit(staking, 'SetRewards').withArgs(now + 10, now + 20,
+                    toETH("500000"), '10')
+                let afterStakingBalance = await rewardToken.balanceOf(staking.address)
+                expect(afterStakingBalance).to.be.equal(beforeStakingBalance.add(toETH("500000")))
+                expect(await staking.startDate()).to.be.equal(now + 10)
+                expect(await staking.endDate()).to.be.equal(now + 20)
+                expect(await staking.apy()).to.be.equal(10)
+            })
+        });
+        describe('stake', async () => {
+            it('should fail if staked amount is zero', async () => {
+                let blo = await ethers.provider.getBlockNumber()
+                let now = (await ethers.provider.getBlock(blo)).timestamp
+                await staking.setRewards(now + 10, now + 20, toETH("500000"), '10')
+                await expect(staking.connect(investor1).stake("0")
+                ).to.be.revertedWith("ERROR_AMOUNT_IS_ZERO")
+            })
+            it('should fail if staked amount is over max', async () => {
+                let blo = await ethers.provider.getBlockNumber()
+                let now = (await ethers.provider.getBlock(blo)).timestamp
+                await staking.setRewards(now + 10, now + 20, toETH("500000"), '10')
+                await stakingToken.mint(investor1.address, toETH("5000001"))
+                await stakingToken.connect(investor1).approve(staking.address, toETH("5000001"))
+                await expect(staking.connect(investor1).stake(toETH("5000001"))
+                ).to.be.revertedWith("ERROR_MAX_TOTAL_SUPPLY")
+            })
+            it('should fail if cooldown has not been passed', async () => {
+                let blo = await ethers.provider.getBlockNumber()
+                let now = (await ethers.provider.getBlock(blo)).timestamp
+                await staking.setRewards(now + 10, now + 20, toETH("500000"), '10')
+                await stakingToken.mint(investor1.address, toETH("5000000"))
+                await stakingToken.connect(investor1).approve(staking.address, toETH("5000000"))
+                await staking.connect(investor1).stake(toETH("2000000"))
+                await expect(staking.connect(investor1).stake(toETH("3000000"))
+                ).to.be.revertedWith("The cooldown has not been passed")
+            })
+            it('should stake the amount of staking tokens successfully', async () => {
+                let blo = await ethers.provider.getBlockNumber()
+                let now = (await ethers.provider.getBlock(blo)).timestamp
+                await staking.setRewards(now + 10, now + 30, toETH("500000"), '10')
+                await stakingToken.mint(investor1.address, toETH("3000"))
+                await stakingToken.connect(investor1).approve(staking.address, toETH("3000"))
+                let beforeStakingBalance = await stakingToken.balanceOf(staking.address)
+                await expect(staking.connect(investor1).stake(toETH("3000"))
+                ).to.emit(staking, 'Stake').withArgs(investor1.address, toETH("3000"))
+                let afterStakingBalance = await stakingToken.balanceOf(staking.address)
+                expect(afterStakingBalance).to.be.equal(beforeStakingBalance.add(toETH("3000")))
+                expect(await staking.totalSupply()).to.be.equal(toETH("3000"))
+                expect(await staking.staked(investor1.address)).to.be.equal(toETH("3000"))
+                expect(await staking.rewards(investor1.address)).to.be.equal(toETH("0"))
+            })
+        });
         describe('unstake', async () => {
             it('should fail if there is no staked tokens', async () => {
                 let blo = await ethers.provider.getBlockNumber()
